61864e02eae917b1e3d2d15f285f9db361a26715
Abdurrahmanalghofiqi <abdurrahmanalghofiqi69@gmail.com>
2024-12-17 21:42:24 +0700

    MahasiswaKt

--- app/src/main/java/com/example/pertemuan10/data/entity/Mahasiswa.kt
+++ app/src/main/java/com/example/pertemuan10/data/entity/Mahasiswa.kt
@@ -1,8 +1,11 @@
 package com.example.pertemuan10.data.entity
 
-import androidx.room.
 
-class Mahasiswa {
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "mahasiswa")
+data class Mahasiswa(
     @PrimaryKey
     val nim : String,
     val nama : String,
@@ -10,5 +13,4 @@ class Mahasiswa {
     val jenisKelamin : String,
     val kelas : String,
     val angkatan : String,
-    )
-}
+)
