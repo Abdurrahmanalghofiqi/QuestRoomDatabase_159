cbb9ae3a0852bf2c97ac108ea6111822b0a015d5
Abdurrahmanalghofiqi <abdurrahmanalghofiqi69@gmail.com>
2024-12-17 21:48:19 +0700

    Repository

--- app/src/main/java/com/example/pertemuan10/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/pertemuan10/repository/LocalRepositoryMhs.kt
@@ -1,7 +1,30 @@
 package com.example.pertemuan10.repository
 
-class LocalRepositoryMhs (
+import com.example.pertemuan10.data.dao.MahasiswaDao
+import com.example.pertemuan10.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositoryMhs(
     private val mahasiswaDao: MahasiswaDao
-    ): RepositoryMhs {
-        override suspend fun insertMhs
+) : RepositoryMhs {
+
+    override suspend fun insertMhs(mahasiswa: Mahasiswa){
+        mahasiswaDao.insertMahasiswa(mahasiswa)
     }
+    // getAllMhs
+    override fun getAllMhs (): Flow<List<Mahasiswa>> {
+        return  mahasiswaDao.getAllMahasiswa()
+    }
+    // getMhs
+    override fun getMhs(nim: String): Flow<Mahasiswa> {
+        return mahasiswaDao.getMahasiswa(nim)
+    }
+    // deletedMhs
+    override suspend fun deletedMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deletedMAhasiswa(mahasiswa)
+    }
+    // updateMhs
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMahasiswa(mahasiswa)
+    }
+}
