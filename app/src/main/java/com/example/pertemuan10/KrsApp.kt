d2b950dddfe7da3c4f815bb49a4b89436f29acc6
Abdurrahmanalghofiqi <abdurrahmanalghofiqi69@gmail.com>
2024-12-17 22:26:02 +0700

    KrsApp

--- app/src/main/java/com/example/pertemuan10/KrsApp.kt
+++ app/src/main/java/com/example/pertemuan10/KrsApp.kt
@@ -1,4 +1,13 @@
 package com.example.pertemuan10
+import android.app.Application
+import com.example.pertemuan10.dependenciesinjection.ContainerApp
 
-class KrsApp {
+class KrsApp : Application() {
+    lateinit var containerApp: ContainerApp // Fungsi untuk menyimpan
+
+    override fun onCreate() {
+        super.onCreate()
+        containerApp = ContainerApp(this) // membuat instance
+        // instance adalah object yanng dibuat dari class
+    }
 }
---
+++ app/src/main/java/com/example/pertemuan10/ui/theme/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.pertemuan10.ui.theme.viewmodel
+
+import android.view.View
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.pertemuan10.KrsApp
+
+
+object  PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            MahasiswaViewModel(
+                KrsApp(). containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            HomeMhsViewModel(
+                KrsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                KrsApp().containerApp.repositoryMhs,
+            )
+        }
+        initializer {
+            UpdateMhsViewModel(
+                createSavedStateHandle(),
+                KrsApp().containerApp.repositoryMhs,
+            )
+        }
+    }
+}
+
+fun CreationExtras.KrsApp(): KrsApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KrsApp)
