fcdf8bf8ecb72297dbd907a30d6749232765bb3d
Abdurrahmanalghofiqi <abdurrahmanalghofiqi69@gmail.com>
2024-12-17 21:40:59 +0700

    interface dan database KrsDatabase

--- app/src/main/java/com/example/pertemuan10/data/database/KrsDatabase.kt
+++ app/src/main/java/com/example/pertemuan10/data/database/KrsDatabase.kt
@@ -1,6 +1,14 @@
 package com.example.pertemuan10.data.database
 
-class KrsDatabase : RoomDatabase() {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.pertemuan10.data.dao.MahasiswaDao
+import com.example.pertemuan10.data.entity.Mahasiswa
+
+@Database(entities = [Mahasiswa::class], version = 1, exportSchema = false)
+abstract class KrsDatabase : RoomDatabase() {
 
     //Mendefinisikan fungsi untuk mengakses data Mahasiswa
 
@@ -10,8 +18,8 @@ class KrsDatabase : RoomDatabase() {
         @Volatile  // Memastikan bahwa nilai variabel instance selalu sama
         private var Instance: KrsDatabase? = null
 
-        fun getDatabase(context: Context): KrsDatabase {
-            return (Instance ?: synchronized(this) {
+        fun getDatabase(context: Context): KrsDatabase{
+            return (Instance ?: synchronized(this){
                 Room.databaseBuilder(
                     context.applicationContext,
                     KrsDatabase::class.java,  // Class database
